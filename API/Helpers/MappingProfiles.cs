da8724e3e504e888e7710f9c4e5f527e453efedc
Demetrix98 <pawlowicz.k@wp.pl>
2020-04-18 13:12:48 +0200

    API - Identity

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,7 +1,8 @@
-using System;
+  
 using API.Dtos;
 using AutoMapper;
 using Core.Entities;
+using Core.Entities.Identity;
 
 namespace API.Helpers
 {
@@ -9,12 +10,11 @@ namespace API.Helpers
     {
         public MappingProfiles()
         {
-            CreateMap<Product,ProductToReturnDto>()
-                .ForMember(d => d.ProductBrand, o => o.MapFrom( s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom( s => s.ProductType.Name))
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+            CreateMap<Address, AddressDto>().ReverseMap();
         }
-
-      
     }
 }
